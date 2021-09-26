1958c6a35f64bd140ef4fa2f697758181543299f
diksha chaudhari <chaudharidiksha4@gmail.com>
2021-09-26 21:09:55 +0530

    make it better

--- config/env.js
+++ config/env.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    //mongoDbUrl: process.env.MONGO_DB_URL || "mongodb://localhost:27017/shoppingcart",
+    mongoDbUrl: process.env.MONGODB_URL || "mongodb://localhost:27017/shoppingcart",
     stripeSecretKey: "sk_test_51JdYuRSAY7edSl7IBmxbHLqhl92bCEr09MqsapS3gMVuVPDU6BfBwAzVjXgXH0vIoXkBMvq3060DwJULc0OXMC2d00dwkWpAOx",
     port: process.env.PORT || 3000
 }
