9fb9742f66e8dec6026d1852d8327eb6037d0bb4
diksha chaudhari <chaudharidiksha4@gmail.com>
2021-09-26 20:43:25 +0530

    make it better

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const csrf = require('csurf');
 const passport = require('passport');
-const contact = require("./models/user");
+const contact = require("../models/user");
 
 const Order = require ('../models/order');
 const Cart = require ('../models/cart');
